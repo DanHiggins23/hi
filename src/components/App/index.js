dd7ffb19d93dd9e75105b2bbe354f82014df60ca
Dan Higgins <dan.higgins@netbuilder.com>
2019-10-10 13:39:20 +0100

    Changed heading font, started work on skills content and added mobile responsive nav bar.

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,89 +1,83 @@
 import React, { Component } from 'react';
 import Nav from '../Nav';
-import Progress from '../Progress';
 import Title from '../Title';
 import './App.scss';
 import '../../styles/_shared.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithubAlt, faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
+import { faLinkedinIn, faGithub } from '@fortawesome/free-brands-svg-icons'
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 export default class App extends Component {
-
-	listenToScrollEvent = () => {
-		document.addEventListener("scroll", () => {
-			requestAnimationFrame(() => {
-				this.calculateScrollDistance();
-			});
-		});
-	};
-
-	componentDidMount() {
-		this.listenToScrollEvent();
-	}
-
-	state = {
-		scrollPosition: 0
-	}
-
-	calculateScrollDistance = () => {
-		const scrollTop = window.pageYOffset;
-		const windowHeight = window.innerHeight;
-		const docHeight = this.getDocHeight();
-
-		const totalDocScrollLength = docHeight - windowHeight;
-		const scrollPosition = Math.ceil(scrollTop / totalDocScrollLength * 100)
-
-		this.setState({
-			scrollPosition,
-		});
-	}
-
-	getDocHeight = () => {
-		return Math.max(
-			document.body.scrollHeight, document.documentElement.scrollHeight,
-			document.body.offsetHeight, document.documentElement.offsetHeight,
-			document.body.clientHeight, document.documentElement.clientHeight
-		);
-	}
-
 	render() {
 		return (
 			<div className="App">
 				<Nav />
-				<Progress scroll={this.state.scrollPosition + "%"} />
 				<div className="section home"></div>
 
 				<div className="section about-me">
-					<Title text="about me" />
+					<Title text="About me" />
 				</div>
 
 				<div className="section projects">
-					<Title text="projects" />
+					<Title text="Projects" />
 				</div>
 
-				<div className="skills skills">
-					<Title text="skills" />
+				<div className="section skills">
+					<Title text="My skills" />
+					<div className="skills__container">
+						<div className="skills__container-item">
+							<img className="skills__icon" src={require('../../utils/images/skills/java.png')} />
+							<span className="skills__link"><a href="#">Java</a></span>
+							<p className="skills__description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+						</div>
+						<div className="skills__container-item">
+							<img className="skills__icon" src={require('../../utils/images/skills/java.png')} />
+							<span className="skills__link"><a href="#">Java</a></span>
+							<p className="skills__description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+						</div>
+						<div className="skills__container-item">
+							<img className="skills__icon" src={require('../../utils/images/skills/java.png')} />
+							<span className="skills__link"><a href="#">Java</a></span>
+							<p className="skills__description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+						</div>
+					</div>
+					<div className="skills__container">
+						<div className="skills__container-item">
+							<img className="skills__icon" src={require('../../utils/images/skills/java.png')} />
+							<span className="skills__link"><a href="#">Java</a></span>
+							<p className="skills__description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+						</div>
+						<div className="skills__container-item">
+							<img className="skills__icon" src={require('../../utils/images/skills/java.png')} />
+							<span className="skills__link"><a href="#">Java</a></span>
+							<p className="skills__description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+						</div>
+						<div className="skills__container-item">
+							<img className="skills__icon" src={require('../../utils/images/skills/java.png')} />
+							<span className="skills__link"><a href="#">Java</a></span>
+							<p className="skills__description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+						</div>
+					</div>
 				</div>
 
 				<div className="section experiences">
-					<Title text="experiences" />
+					<Title text="Experiences" />
 				</div>
 
 				<div className="section contact-me">
-					<Title text="contact me" />
+					<Title text="Contact me" />
 					<div className="contact-me__container">
 						<div className="contact-me__container-item">
-							<FontAwesomeIcon icon={faGithubAlt} />
-							<span><a className="contact-me__link" href="#">DanHiggins23</a></span>
+							<FontAwesomeIcon icon={faGithub} />
+							<span className="contact-me__link"><a href="#">DanHiggins23</a></span>
 						</div>
 						<div className="contact-me__container-item">
 							<FontAwesomeIcon icon={faEnvelope} />
-							<span><a className="contact-me__link" href="#">danhiggins23@outlook.com</a></span>
+							<span className="contact-me__link"><a href="#">danhiggins23@outlook.com</a></span>
 						</div>
 						<div className="contact-me__container-item">
 							<FontAwesomeIcon icon={faLinkedinIn} />
-							<span><a className="contact-me__link" href="#">Dan Higgins</a></span>
+							<span className="contact-me__link"><a href="#">Dan Higgins</a></span>
 						</div>
 					</div>
 				</div>
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,17 +1,95 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Progress from '../Progress';
 import './Nav.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
-export default function Nav(props) {
-    return (
-        <div className="nav">
-            <h1 className="nav__logo">DH</h1>
-            <ul className="nav__list">
-                <li className="nav__list-items"><a className="nav__list-items-link" href="#">About</a></li>
-                <li className="nav__list-items"><a className="nav__list-items-link" href="#">Projects</a></li>
-                <li className="nav__list-items"><a className="nav__list-items-link" href="#">Skills</a></li>
-                <li className="nav__list-items"><a className="nav__list-items-link" href="#">Experiences</a></li>
-                <li className="nav__list-items"><a className="nav__list-items-link" href="#">Contact</a></li>
-            </ul>
-        </div>
-    )
+export default class Nav extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { 
+            display: false,
+            scrollPosition: 0
+        }
+    }
+
+    handleClick = () => {
+        this.setState({
+            display: !this.state.display
+        });
+    }
+
+    listenToScrollEvent = () => {
+		document.addEventListener("scroll", () => {
+			requestAnimationFrame(() => {
+				this.calculateScrollDistance();
+			});
+		});
+	};
+
+	componentDidMount() {
+		this.listenToScrollEvent();
+	}
+
+	calculateScrollDistance = () => {
+		const scrollTop = window.pageYOffset;
+		const windowHeight = window.innerHeight;
+		const docHeight = this.getDocHeight();
+
+		const totalDocScrollLength = docHeight - windowHeight;
+		const scrollPosition = Math.ceil(scrollTop / totalDocScrollLength * 100)
+
+		this.setState({
+			scrollPosition,
+		});
+	}
+
+	getDocHeight = () => {
+		return Math.max(
+			document.body.scrollHeight, document.documentElement.scrollHeight,
+			document.body.offsetHeight, document.documentElement.offsetHeight,
+			document.body.clientHeight, document.documentElement.clientHeight
+		);
+	}
+
+    render() {
+        return (
+            <div>
+                <div className="nav">
+                    <h1 className="nav__logo">DH</h1>
+                    <ul className="nav__list">
+                        <li className="nav__list-items nav__list-items--desktop">
+                            <a className="nav__list-items-link" href="#">ABOUT</a>
+                        </li>
+                        <li className="nav__list-items nav__list-items--desktop">
+                            <a className="nav__list-items-link" href="#">PROJECTS</a>
+                        </li>
+                        <li className="nav__list-items nav__list-items--desktop">
+                            <a className="nav__list-items-link" href="#">SKILLS</a>
+                        </li>
+                        <li className="nav__list-items nav__list-items--desktop">
+                            <a className="nav__list-items-link" href="#">EXPERIENCES</a>
+                        </li>
+                        <li className="nav__list-items nav__list-items--desktop">
+                            <a className="nav__list-items-link" href="#">CONTACT</a>
+                        </li>
+                    </ul>
+                    <FontAwesomeIcon onClick={this.handleClick} className="nav__hamburger" icon={faBars} />
+                </div>
+                {this.state.display === true ?
+                    <div className="container">
+                        <ul className="list">
+                            <li className="nav__list-items"><a className="nav__list-items-link" href="#">ABOUT</a></li>
+                            <li className="nav__list-items"><a className="nav__list-items-link" href="#">PROJECTS</a></li>
+                            <li className="nav__list-items"><a className="nav__list-items-link" href="#">SKILLS</a></li>
+                            <li className="nav__list-items"><a className="nav__list-items-link" href="#">EXPERIENCES</a></li>
+                            <li className="nav__list-items"><a className="nav__list-items-link" href="#">CONTACT</a></li>
+                        </ul>
+                    </div>
+                    : null}
+                <Progress scroll={this.state.scrollPosition + "%"} />
+            </div>
+        );
+    };
 }
