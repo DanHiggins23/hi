96bbde8763e18787532fcaf749a52347b13e9fd4
Dan Higgins <dan.higgins@netbuilder.com>
2019-10-10 16:24:09 +0100

    Updates

---
+++ precache-manifest.eb9c54fe0da2bb1c55581f9101abfeae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "167c7829421332204671cf4294010f0d",
+    "url": "/DanHiggins23/my-portfolio/index.html"
+  },
+  {
+    "revision": "831ef744dcdaa4fc3625",
+    "url": "/DanHiggins23/my-portfolio/static/css/main.5d0389bd.chunk.css"
+  },
+  {
+    "revision": "083c5b3a7bf120d9ef0d",
+    "url": "/DanHiggins23/my-portfolio/static/js/2.2dfa8182.chunk.js"
+  },
+  {
+    "revision": "831ef744dcdaa4fc3625",
+    "url": "/DanHiggins23/my-portfolio/static/js/main.d16bc492.chunk.js"
+  },
+  {
+    "revision": "f06ead590bb259fe5ad5",
+    "url": "/DanHiggins23/my-portfolio/static/js/runtime~main.c8afa701.js"
+  },
+  {
+    "revision": "7c17a5f1195c693a155f6406288bd277",
+    "url": "/DanHiggins23/my-portfolio/static/media/java.7c17a5f1.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "danhiggins.xyz/precache-manifest.be2465dbda3b54f6f0dec28490c2efe8.js"
+  "/DanHiggins23/my-portfolio/precache-manifest.eb9c54fe0da2bb1c55581f9101abfeae.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("danhiggins.xyz/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/DanHiggins23/my-portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
