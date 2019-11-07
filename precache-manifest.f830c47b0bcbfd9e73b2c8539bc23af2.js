9bad5869825ab8711d894f0f359ed589dda1062d
Dan Higgins <44192413+DanHiggins23@users.noreply.github.com>
2019-11-07 12:11:51 +0000

    Updates

---
+++ precache-manifest.f830c47b0bcbfd9e73b2c8539bc23af2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef5ea507627b924202f43b59cf87dabc",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "d6c2f5861722c5ab413a",
+    "url": "/my-portfolio/static/css/main.f44ca21f.chunk.css"
+  },
+  {
+    "revision": "7be73be0b2a782c94702",
+    "url": "/my-portfolio/static/js/2.e6112aa1.chunk.js"
+  },
+  {
+    "revision": "d6c2f5861722c5ab413a",
+    "url": "/my-portfolio/static/js/main.8a73e7ff.chunk.js"
+  },
+  {
+    "revision": "f2d15970f74596a69ca7",
+    "url": "/my-portfolio/static/js/runtime~main.674e5ed7.js"
+  },
+  {
+    "revision": "1b0122e5f030346b0703d930b7cf850a",
+    "url": "/my-portfolio/static/media/DanHiggins.1b0122e5.pdf"
+  },
+  {
+    "revision": "fbf3ad9e00ef0538bd7349bb3eb6d0f8",
+    "url": "/my-portfolio/static/media/QA-logo.fbf3ad9e.png"
+  },
+  {
+    "revision": "5c8f04b30e43aba530fa35f974be97a4",
+    "url": "/my-portfolio/static/media/bt-logo.5c8f04b3.png"
+  },
+  {
+    "revision": "919a39e1793a404f7e852e02cb53088b",
+    "url": "/my-portfolio/static/media/ford-logo.919a39e1.png"
+  },
+  {
+    "revision": "fd3af5c9d0e816b85048514e8be5c179",
+    "url": "/my-portfolio/static/media/graduation.fd3af5c9.jpg"
+  },
+  {
+    "revision": "b2f8a129cc6659cbfbfffd5abc96003e",
+    "url": "/my-portfolio/static/media/nb-logo.b2f8a129.png"
+  }
+]);
