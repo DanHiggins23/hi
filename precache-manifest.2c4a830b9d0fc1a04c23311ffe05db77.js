07e8bde1f55955d79e8631c5ce587e7ebc4647cc
DanHiggins23 <danhiggins23@outlook.com>
2021-09-01 22:52:32 +0100

    Updates

---
+++ precache-manifest.2c4a830b9d0fc1a04c23311ffe05db77.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "251dc1f1f3acaf760cd67e45f32f4c7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4164ce3824bac6e7e344",
+    "url": "/static/css/main.6a44c6c9.chunk.css"
+  },
+  {
+    "revision": "e03254920444665d116c",
+    "url": "/static/js/2.7d8dfe91.chunk.js"
+  },
+  {
+    "revision": "4164ce3824bac6e7e344",
+    "url": "/static/js/main.dc913288.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "06e73f32cec3f1e1f606f53ac872b89b",
+    "url": "/static/media/DanHiggins.06e73f32.pdf"
+  },
+  {
+    "revision": "fbf3ad9e00ef0538bd7349bb3eb6d0f8",
+    "url": "/static/media/QA-logo.fbf3ad9e.png"
+  },
+  {
+    "revision": "50b1e498918964d8f96ae1dae46828ce",
+    "url": "/static/media/amsterdam.50b1e498.jpg"
+  },
+  {
+    "revision": "5c8f04b30e43aba530fa35f974be97a4",
+    "url": "/static/media/bt-logo.5c8f04b3.png"
+  },
+  {
+    "revision": "f6f24dfc6f097a4aa2c8bf0444787d47",
+    "url": "/static/media/ford-logo-trans.f6f24dfc.png"
+  },
+  {
+    "revision": "919a39e1793a404f7e852e02cb53088b",
+    "url": "/static/media/ford-logo.919a39e1.png"
+  },
+  {
+    "revision": "b2f8a129cc6659cbfbfffd5abc96003e",
+    "url": "/static/media/nb-logo.b2f8a129.png"
+  },
+  {
+    "revision": "e1a632c3e14928dbaeab44601699f220",
+    "url": "/static/media/open-university.e1a632c3.png"
+  },
+  {
+    "revision": "602a604c22be81420a5c6a6f031c5394",
+    "url": "/static/media/santander-logo.602a604c.png"
+  },
+  {
+    "revision": "faeb3ef8cf3c80bcdf414d4e8329adf4",
+    "url": "/static/media/teesside-uni.faeb3ef8.png"
+  },
+  {
+    "revision": "58cdab0fa966b171d09f203ecb5b7472",
+    "url": "/static/media/thg-logo.58cdab0f.png"
+  }
+]);
