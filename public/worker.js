e9fb55fda2b17a74d2e05491a2274c69a5a9cb47
DanHiggins23 <danhiggins23@outlook.com>
2023-05-25 14:28:23 +0100

    Adding highlight colour variable and tweaks to service worker

--- public/worker.js
+++ public/worker.js
@@ -1,42 +1,42 @@
-var CACHE_NAME = "hi";
-var urlsToCache = ["/", "/completed"];
+/* eslint-disable no-restricted-globals */
+const CACHE_NAME = 'hi';
+const urlsToCache = ['/'];
 
 // Install a service worker
-self.addEventListener("install", (event) => {
+self.addEventListener('install', event => {
   // Perform install steps
   event.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      console.log("Opened cache");
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('Opened cache');
       return cache.addAll(urlsToCache);
-    })
+    }),
   );
 });
 
 // Cache and return requests
-self.addEventListener("fetch", (event) => {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(function (response) {
+    caches.match(event.request).then(response => {
       // Cache hit - return response
       if (response) {
         return response;
       }
       return fetch(event.request);
-    })
+    }),
   );
 });
 
 // Update a service worker
-self.addEventListener("activate", (event) => {
-  var cacheWhitelist = ["my-portfolio"];
+self.addEventListener('activate', event => {
+  const cacheWhitelist = ['my-portfolio'];
   event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
+    caches.keys().then(cacheNames => Promise.all(
+      // eslint-disable-next-line array-callback-return, consistent-return
+      cacheNames.map(cacheName => {
+        if (cacheWhitelist.indexOf(cacheName) === -1) {
+          return caches.delete(cacheName);
+        }
+      }),
+    )),
   );
 });
